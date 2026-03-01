881b5fa3a7070fd710cf44fe8a9098378ad4fed1
Thevindu23 <thevinduariyathilaka@gmail.com>
2026-03-01 20:12:54 +0530

    first commit

---
+++ teago/src/main/java/com/teago/teago/TeagoApplication.java
@@ -0,0 +1,13 @@
+package com.teago.teago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeagoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeagoApplication.class, args);
+	}
+
+}
---
+++ teago/src/test/java/com/teago/teago/TeagoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teago.teago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeagoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
