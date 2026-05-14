f9f22509db719345404bbb005166aa22e3d2e0f7
Ruveena Gamage <ruveenasinali@gmail.com>
2026-05-15 05:13:39 +0530

    changes

--- teago/src/main/java/com/teago/teago/models/FactoryOwner.java
+++ teago/src/main/java/com/teago/teago/models/FactoryOwner.java
@@ -1,8 +1,16 @@
 package com.teago.teago.models;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+@Getter
+@Setter
+@Table(name = "FactoryOwner")
 public class FactoryOwner {
 
-}
     @Id
     @Column(name = "FactoryOwnerID")
     private Integer factoryOwnerId;
@@ -12,21 +20,19 @@ public class FactoryOwner {
     @JoinColumn(name = "FactoryOwnerID")
     private User user;
 
-
+    /** Full name of the factory owner */
     @Column(name = "FullName", length = 150)
     private String fullName;
 
+    /** Official factory / company name */
     @Column(name = "FactoryName", length = 150)
     private String factoryName;
 
-   
+    /** Physical location of the factory */
     @Column(name = "FactoryLocation", length = 255)
     private String factoryLocation;
 
-   
+    /** Official business / company registration number */
     @Column(name = "RegistrationNumber", length = 100, unique = true)
     private String registrationNumber;
 }
-
-
-    
