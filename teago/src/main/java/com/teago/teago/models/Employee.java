e7af1472df907e90a663114fa2870b9da3ca819a
Thevindu23 <thevinduariyathilaka@gmail.com>
2026-05-15 04:46:29 +0530

    Employees Function

--- teago/src/main/java/com/teago/teago/models/Employee.java
+++ teago/src/main/java/com/teago/teago/models/Employee.java
@@ -1,5 +1,39 @@
-package com.teago.teago.models;
+package com.teago.teago.Models;
 
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Employee")
+@Getter
+@Setter
 public class Employee {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "EmployeeID")
+    private Integer employeeId;
+
+    @ManyToOne
+    @JoinColumn(name = "LandOwnerID")
+    private LandOwner landOwner;
+
+    @Column(name = "EmployeeName", length = 100)
+    private String employeeName;
+
+    @Column(name = "WorkDays")
+    private Integer workDays;
+
+    @Column(name = "SalaryRate", precision = 8, scale = 2)
+    private BigDecimal salaryRate;
 }
